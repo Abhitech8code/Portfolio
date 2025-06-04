d942b1aee0953f1d5e5f176e97ea6044ae52762b
Abhitech8code <86166003+Abhitech8code@users.noreply.github.com>
2025-06-05 00:47:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -196,7 +196,7 @@ const App = () => {
       icon: <Award className="w-10 h-10 text-yellow-500 dark:text-yellow-400" />,
       link: "#", 
       description: "Practical and Theoretical understanding of IT ,Web Technology, C Language, ICT  Fundamentals.",
-      imageSrc: "/public/OLEVEL.jpg",
+      imageSrc: "https://img.freepik.com/premium-photo/cyber-boy-pictures-cute-cyber-boy-image-download-hd_846390-3657.jpg?uid=R157439276&ga=GA1.1.390215850.1702361898&semt=ais_items_boosted&w=740",
       imageAlt: "'O' Level Diploma Degree",
     },
     {
